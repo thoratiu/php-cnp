49c4254eb577bea6b28c787711451bb309e31ffd
thoratiu <80838708+thoratiu@users.noreply.github.com>
2021-03-17 17:13:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <html>
     <body>
+        
 <form action="index.php" method="post">
 <label >Please insert your Identification Number "CNP":</label>
 <input type="text" target="_self"  name="cnp"  autofocus>
-
-
 <input type="submit">
-
-
-
 </form>
+
 <!-- we can use HTML to force input field to be a number and to limit character to 13 but I will use php to accomplish that <input type="number" maxlength="13" min="1000101010011" max="9991231529999" required autofocus> -->
 
 
 <?php
-echo "<br>Your insert was:".$_POST["cnp"];
+echo "<br>Your insert is: ".$_POST["cnp"];
 
 //we have to clean the insert for security reasons (suplimentary can be used trim() and stripslashes() ) but for the moment I will use only htmlspecialchars()
 
-//$cnp = htmlspecialchars($_POST["cnp"]);
+$cnp = htmlspecialchars($_POST["cnp"]);
 
-$cnp = $_POST["cnp"];
-
+//check inserted value before verification
 if(empty($cnp))
 echo "<br>Please insert 13 numeric characters - your insert is empty";
 elseif(!is_numeric($cnp))
 echo "<br>Please insert a numeric value - your insert contains a string character or is not a integer";
 elseif(strlen($cnp)!=13)
-echo "<br>lease insert 13 numeric characters. You inserted ".strlen($cnp)." characters";
+echo "<br>Please insert 13 numeric characters. You inserted ".strlen($cnp)." characters";
 else
 {
     // test CNP number
     $ControlNumber=279146358279;
+    $prod=0;
+    
+    echo "<br><b> Verification Started </b>";
+    
     
     for($a=0;$a<=11;$a++)
     {
@@ -55,9 +55,9 @@ else
     $Modulo=1;
     
     if($Modulo==$last_char)
-    echo "<br><b>CNP is VALID";
+    echo "<br><br><b>CNP is VALID";
     else
-    echo "<br><b>CNP is INVALID";
+    echo "<br><br><b>CNP is INVALID";
     
     
 }
